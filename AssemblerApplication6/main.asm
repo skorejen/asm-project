da5a519b3dd992d9954e122e95eb00fd4a28e4a7
Michal Ciebien <skorejen@gmail.com>
2018-11-26 10:38:28 +0100

    Refactored code

--- AssemblerApplication6/main.asm
+++ AssemblerApplication6/main.asm
@@ -14,8 +14,11 @@ JMP MAIN
  .ORG 0x02 ; vector interrupt for INT0 = 0x02
 	call int_detect_wave_from_sensor
 
+
+	;----- start main
+		.ORG 0x100 ; ommit first 100 addresses left for interrupts
 	main :
-	.ORG 0x100
+
 	LDI R16, HIGH(RAMEND)
 	OUT SPH, R16
 	LDI R16, LOW(RAMEND)
@@ -37,22 +40,21 @@ JMP MAIN
 	clr r16
 
 	ldi r16, 1<<PA0
-	out PORTA, r16 // Send HIGH on PA0 to start the detect
+	out PORTA, r16 // Send HIGH on PA0 to start the detect and PROBABLY start interrupt
 	ldi r16, 0
 	out PORTA, r16 // clear the PA0
 	clr r16
 
 
-	; get data from ultrasonic sensor
+	
 
 	call delay_detection_60ms
-	
-	
-	; sbic PIND, PD7  ; get high from aultrasonic sensor
-	
-	ldi r21, 1<<PD0
 
-	delay_detection_60ms: ; delay by 750x800x32 = 19.2 10^6 clock cycles, which is  around 1.2s
+    rjmp main
+	; ----- end main
+
+
+		delay_detection_60ms: ; delay 60ms for the sensor accurate response
 	ldi r17, 250
 	
 	again:
@@ -63,18 +65,13 @@ JMP MAIN
 	dec r17
 	brne again2
 	ret
-	
-		; R20 responsible for holding data time for buzzer
-	; send High Time
-	
-    rjmp main
 
 	
 	int_detect_wave_from_sensor:
 
 	 ldi r19, 1<<PA2 ; high on PA2 for buzzer 
 	 out PORTA, r19
-	 call delay_detection_60ms
+	 call delay_detection_60ms ; just a random delay that would put buzz at least for 60ms
 	 clr r19
 	 reti
 
